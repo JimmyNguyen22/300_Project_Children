2dcd543143380f2b9250efa6a7158d00f1bb0fd6
jimmy <vphu456@gmail.com>
2022-08-17 22:12:28 +0700

    image gallery

--- js/modal.js
+++ js/modal.js
@@ -15,3 +15,61 @@ modal.addEventListener("click", (event) => {
     toggleModal();
   }
 });
+
+// img show
+let img = document.querySelectorAll(".image img");
+let prev = document.querySelector(".prev");
+let right = document.querySelector(".right");
+let close = document.querySelector(".close");
+let galleryInner = document.querySelector(".gallery-inner img");
+let gallery = document.querySelector(".gallery");
+
+let currentIndex = 0;
+
+function showGallery() {
+  if (currentIndex == 0) {
+    prev.classList.add("hide-prev");
+  } else {
+    prev.classList.remove("hide-prev");
+  }
+
+  if (currentIndex == img.length - 1) {
+    right.classList.add("hide-prev");
+  } else {
+    right.classList.remove("hide-prev");
+  }
+  // display
+  galleryInner.src = img[currentIndex].src;
+  gallery.classList.add("show");
+}
+
+img.forEach((item, index) => {
+  item.addEventListener("click", function () {
+    currentIndex = index;
+    showGallery();
+  });
+});
+
+close.addEventListener("click", function () {
+  gallery.classList.remove("show");
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.keyCode == 27) {
+    gallery.classList.remove("show");
+  }
+});
+
+prev.addEventListener("click", function () {
+  if (currentIndex > 0) {
+    currentIndex--;
+    showGallery();
+  }
+});
+
+right.addEventListener("click", function () {
+  if (currentIndex < img.length - 1) {
+    currentIndex++;
+    showGallery();
+  }
+});
